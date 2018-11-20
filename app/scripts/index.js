5deb6dfbfd9c01ee6d665e392c889cd800d394c8
jlombana <jlombana@hugeinc.com>
2018-11-20 16:47:59 -0500

    8. indexedDB and dynamic data

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,6 @@
 import template from "../template/news-template.html";
 import "./register";
+import database from "../utils/idb-database";
 
 const url = "https://news-d62a0.firebaseio.com/news.json";
 let networkDataReceived = false;
@@ -22,26 +23,15 @@ fetch(url)
     document.querySelector(".news-container").innerHTML = html;
   });
 
-if ("caches" in window) {
-  caches
-    .match(url)
-    .then(res => {
-      if (res) {
-        return res.json();
-      }
-    })
-    .then(data => {
-      console.log("From cache", data);
-      if (!networkDataReceived) {
-        let fetchData = {
-          news: []
-        };
-
-        for (let item in data) {
-          fetchData.news.push(data[item]);
-        }
-        const html = template(fetchData);
-        document.querySelector(".news-container").innerHTML = html;
-      }
-    });
+if ("indexedDB" in window) {
+  database.getAllData("news").then(data => {
+    if (!networkDataReceived) {
+      console.log("from cache", data);
+      let cacheData = {
+        news: data
+      };
+      const html = template(cacheData);
+      document.querySelector(".news-container").innerHTML = html;
+    }
+  });
 }
